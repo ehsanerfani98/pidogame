55062acfab8a7cf32cf24320b329186a1d840969
Your Name <you@example.com>
2022-07-12 13:43:00 +0430

    edit cart

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ function pidoGameEditorFiles()
 {
 	add_editor_style(get_template_directory_uri() . '/assets/custom/css/admin.bundle.min.css');
 	add_editor_style(get_template_directory_uri() . '/assets/custom/css/iransansx.min.css');
-	add_image_size( 'cart-product-plswb', 300, 150, true );
+	add_image_size('cart-product-plswb', 300, 150, true);
 }
 add_action('after_setup_theme', 'pidoGameEditorFiles');
 
@@ -675,7 +675,7 @@ function addProductAttributeFilter($attribute, $i = 0)
 			</label>
 		</td>
 	</tr>
-<?php
+	<?php
 }
 add_action('woocommerce_after_product_attribute_settings', 'addProductAttributeFilter', 10, 2);
 
@@ -695,40 +695,45 @@ function ajaxWoocommerceSaveAttributes()
 add_action('wp_ajax_woocommerce_save_attributes', 'ajaxWoocommerceSaveAttributes', 0);
 
 
-function dd($dd){
+function dd($dd)
+{
 	wp_die(var_dump($dd));
 }
 
-add_action( 'woocommerce_add_to_cart', function ()
-{
-  add_action( 'myalarm', function(){
+add_action('woocommerce_add_to_cart', function () {
+	add_action('myalarm', function () {
 
-?>
-<!--begin::Alert-->
-<div class="alert alert-dismissible bg-primary d-flex flex-column flex-sm-row p-5 mb-10">
-    <!--begin::Icon-->
-    <span class="svg-icon svg-icon-2hx svg-icon-light me-4 mb-5 mb-sm-0">...</span>
-    <!--end::Icon-->
-
-    <!--begin::Wrapper-->
-    <div class="d-flex flex-column text-light pe-0 pe-sm-10">
-        <!--begin::Title-->
-        <h4 class="mb-2 light">This is an alert</h4>
-        <!--end::Title-->
-
-        <!--begin::Content-->
-        <span>The alert component can be used to highlight certain parts of your page for higher content visibility.</span>
-        <!--end::Content-->
-    </div>
-    <!--end::Wrapper-->
-
-    <!--begin::Close-->
-    <button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto" data-bs-dismiss="alert">
-        <span class="svg-icon svg-icon-2x svg-icon-light">...</span>
-    </button>
-    <!--end::Close-->
-</div>
-<!--end::Alert-->
+	?>
+		<div class="alert alert-dismissible bg-info d-flex flex-column flex-sm-row w-100 p-5 mb-10">
+			<!--begin::Icon-->
+			<!--begin::Svg Icon | path: icons/duotune/general/gen007.svg-->
+			<span class="svg-icon svg-icon-2hx svg-icon-light me-4 mb-5 mb-sm-0">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+					<path opacity="0.3" d="M12 22C13.6569 22 15 20.6569 15 19C15 17.3431 13.6569 16 12 16C10.3431 16 9 17.3431 9 19C9 20.6569 10.3431 22 12 22Z" fill="black"></path>
+					<path d="M19 15V18C19 18.6 18.6 19 18 19H6C5.4 19 5 18.6 5 18V15C6.1 15 7 14.1 7 13V10C7 7.6 8.7 5.6 11 5.1V3C11 2.4 11.4 2 12 2C12.6 2 13 2.4 13 3V5.1C15.3 5.6 17 7.6 17 10V13C17 14.1 17.9 15 19 15ZM11 10C11 9.4 11.4 9 12 9C12.6 9 13 8.6 13 8C13 7.4 12.6 7 12 7C10.3 7 9 8.3 9 10C9 10.6 9.4 11 10 11C10.6 11 11 10.6 11 10Z" fill="black"></path>
+				</svg>
+			</span>
+			<!--end::Svg Icon-->
+			<!--end::Icon-->
+			<!--begin::Content-->
+			<div class="d-flex flex-column text-light pe-0 pe-sm-10">
+				<h4 class="mb-2 text-light">تبریک</h4>
+				<span>محصول شما با موفقیت به سبد خرید اضافه شد.</span>
+			</div>
+			<!--end::Content-->
+			<!--begin::Close-->
+			<button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto" data-bs-dismiss="alert">
+				<!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+				<span class="svg-icon svg-icon-2x svg-icon-light">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+						<rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black"></rect>
+						<rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black"></rect>
+					</svg>
+				</span>
+				<!--end::Svg Icon-->
+			</button>
+			<!--end::Close-->
+		</div>
 <?php
-});
+	});
 });
