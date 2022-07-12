21434b32e7d251c72ec8949a02a37201616d8eb3
Your Name <you@example.com>
2022-07-12 13:15:38 +0430

    edit cart

--- ajax/remove-cart-product.php
+++ ajax/remove-cart-product.php
@@ -3,7 +3,7 @@ require_once("../../../../wp-load.php");
 
 $productId = $_POST['productId'];
 foreach (WC()->cart->get_cart() as $cartItemKey => $cartItem) {
-    if ($cartItem['product_id'] == $productId) {
+    if ($cartItem['variation_id'] == $productId) {
         WC()->cart->remove_cart_item($cartItemKey);
     }
 }
