869cd4b9950d0340d4ac1ac61c44b98be4fc2722
Your Name <you@example.com>
2022-07-12 13:02:28 +0430

    edit cart

--- ajax/get-header-cart.php
+++ ajax/get-header-cart.php
@@ -53,10 +53,10 @@ $options = get_option('pidogame_framework') ?>
                                             </svg>
                                         </span>
                                         <span class="fs-8 text-primary">
-                                            ریجن <?php
+                                            <?php
                                                     $slug_r = $values['variation']['attribute_pa_region'];
                                                     $slug_d = $values['variation']['attribute_pa_platform'];
-                                                    echo get_term_by('slug', $slug_d, 'pa_platform')->name . ' ' . get_term_by('slug', $slug_r, 'pa_region')->name;
+                                                    echo get_term_by('slug', $slug_d, 'pa_platform')->name . ' ریجن ' . get_term_by('slug', $slug_r, 'pa_region')->name;
                                                     ?>
                                         </span>
                                     </div>
