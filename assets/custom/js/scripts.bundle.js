118ec95d37134cfb4cbb82d3448dce77dc313ea7
Your Name <you@example.com>
2022-07-18 18:57:07 +0430

    test cart

--- assets/custom/js/scripts.bundle.js
+++ assets/custom/js/scripts.bundle.js
@@ -411,20 +411,20 @@ jQuery(function () {
     }
 
     // Change attributes on modal (Buy product modal)
-    var productBuyModal = document.getElementById('kt_modal_product_buy');
-    if (productBuyModal) {
-        productBuyModal.addEventListener('show.bs.modal', function (event) {
-            var button = event.relatedTarget;
-            $(button).each(function () {
-                $.each(this.attributes, function () {
-                    if (this.specified && this.name.startsWith('data-bs-attribute')) {
-                        var recipient = this.name.replace('data-bs-', '');
-                        $('#kt_modal_product_buy').find('[name="' + recipient + '"]').val(this.value).trigger('change');
-                    }
-                });
-            });
-        })
-    }
+    // var productBuyModal = document.getElementById('kt_modal_product_buy');
+    // if (productBuyModal) {
+    //     productBuyModal.addEventListener('show.bs.modal', function (event) {
+    //         var button = event.relatedTarget;
+    //         $(button).each(function () {
+    //             $.each(this.attributes, function () {
+    //                 if (this.specified && this.name.startsWith('data-bs-attribute')) {
+    //                     var recipient = this.name.replace('data-bs-', '');
+    //                     $('#kt_modal_product_buy').find('[name="' + recipient + '"]').val(this.value).trigger('change');
+    //                 }
+    //             });
+    //         });
+    //     })
+    // }
 
     // Reply comment
     $('.reply-comment').click(function () {
