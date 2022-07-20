b243d11266fe0423e1aafd6000d1c4a8f14bdfe1
Your Name <you@example.com>
2022-07-20 12:52:10 +0430

    test javascript

--- functions.php
+++ functions.php
@@ -4,6 +4,12 @@
 require_once get_theme_file_path() . '/codestar/codestar-framework.php';
 require_once get_theme_file_path() . '/options.php';
 
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
 
 // Get theme mode
 function getThemeMode()
---
+++ woocommerce/content-single-product.php
@@ -0,0 +1,21 @@
+<?php
+$options = get_option('pidogame_framework');
+$meta = get_post_meta(get_the_ID(), 'pidogame_framework_products', true);
+?>
+
+                    <?php
+                    $type = $meta['opt-product-type'];
+                    switch ($type) {
+                        case 'game':
+                            get_template_part('templates/product/game/game');
+                            break;
+                        case 'gift':
+                            get_template_part('templates/product/gift/gift');
+                            break;
+                        case 'item':
+                            get_template_part('templates/product/item/item');
+                            break;
+                    }
+                    ?>
+     
+<?php
