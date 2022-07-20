0d32f1698c81949589e59af1cecc1529306ba03e
Your Name <you@example.com>
2022-07-20 11:00:14 +0430

    test javascript

--- footer.php
+++ footer.php
@@ -7,23 +7,7 @@
 <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/custom/fslightbox/fslightbox.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/js/scripts.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/custom/js/scripts.bundle.js"></script>
-<script>
-       // Change attributes on modal (Buy product modal)
-       var productBuyModal = document.getElementById('kt_modal_product_buy');
-    if (productBuyModal) {
-        productBuyModal.addEventListener('show.bs.modal', function (event) {
-            var button = event.relatedTarget;
-            $(button).each(function () {
-                $.each(this.attributes, function () {
-                    if (this.specified && this.name.startsWith('data-bs-attribute')) {
-                        var recipient = this.name.replace('data-bs-', '');
-                        $('#kt_modal_product_buy').find('[name="' + recipient + '"]').val(this.value).trigger('change');
-                    }
-                });
-            });
-        })
-    }
-</script>
+
 
 <?php wp_footer() ?>
 </body>
