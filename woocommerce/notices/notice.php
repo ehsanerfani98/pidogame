6e975489f2d756d8c095d0316cfbdd2d30f8820a
Your Name <you@example.com>
2022-07-20 21:56:21 +0430

    cart edit

--- woocommerce/notices/notice.php
+++ woocommerce/notices/notice.php
@@ -45,18 +45,7 @@ if (!$notices) {
 			</span>
 		</div>
 		<!--end::Content-->
-		<!--begin::Close-->
-		<button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto" data-bs-dismiss="alert">
-			<!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
-			<span class="svg-icon svg-icon-1 svg-icon-info">
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-					<rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black"></rect>
-					<rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black"></rect>
-				</svg>
-			</span>
-			<!--end::Svg Icon-->
-		</button>
-		<!--end::Close-->
+		
 	</div>
 	<!-- <div class="woocommerce-info" <?php echo wc_get_notice_data_attr($notice); ?>>
 		<?php echo wc_kses_notice($notice['notice']); ?>
