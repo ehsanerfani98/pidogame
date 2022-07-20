6efbd388da8f24cc391d2886c75d2772c0a72d99
Your Name <you@example.com>
2022-07-20 11:26:50 +0430

    test javascript

--- footer.php
+++ footer.php
@@ -3,10 +3,10 @@
     var templateDirectory = "<?php echo get_template_directory_uri() ?>";
 </script>
 
-<script src="<?php echo get_template_directory_uri() ?>/assets/plugins/global/plugins.bundle.js"></script>
+<!-- <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/global/plugins.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/custom/fslightbox/fslightbox.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/js/scripts.bundle.js"></script>
-<script src="<?php echo get_template_directory_uri() ?>/assets/custom/js/scripts.bundle.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>/assets/custom/js/scripts.bundle.js"></script> -->
 
 
 <?php wp_footer() ?>
