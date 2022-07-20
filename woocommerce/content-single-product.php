efd67f1ad926442e6e6b3b6e079449d1c1555c74
Your Name <you@example.com>
2022-07-20 12:56:06 +0430

    test javascript

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -4,18 +4,18 @@ $meta = get_post_meta(get_the_ID(), 'pidogame_framework_products', true);
 ?>
 
                     <?php
-                    $type = $meta['opt-product-type'];
-                    switch ($type) {
-                        case 'game':
-                            get_template_part('templates/product/game/game');
-                            break;
-                        case 'gift':
-                            get_template_part('templates/product/gift/gift');
-                            break;
-                        case 'item':
-                            get_template_part('templates/product/item/item');
-                            break;
-                    }
-                    ?>
+					$type = $meta['opt-product-type'];
+					switch ($type) {
+						case 'game':
+							get_template_part('templates/product/game/game');
+							break;
+						case 'gift':
+							get_template_part('templates/product/gift/gift');
+							break;
+						case 'item':
+							get_template_part('templates/product/item/item');
+							break;
+					}
+					?>
      
 <?php
