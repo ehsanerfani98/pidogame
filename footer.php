a1d9a3806af582b3b9c34ae9b15d8ffc748fb7f9
Your Name <you@example.com>
2022-07-20 10:50:09 +0430

    test javascript

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
     var hostUrl = "assets/";
     var templateDirectory = "<?php echo get_template_directory_uri() ?>";
 </script>
-<script src="<?php echo get_template_directory_uri() ?>/assets/plugins/global/plugins.bundle.js"></script>
+<!-- <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/global/plugins.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/custom/fslightbox/fslightbox.bundle.js"></script>
-<script src="<?php echo get_template_directory_uri() ?>/assets/js/scripts.bundle.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/scripts.bundle.js"></script> -->
 <script src="<?php echo get_template_directory_uri() ?>/assets/custom/js/scripts.bundle.js"></script>
 
 
