45aa9c9f86fb5f5917582e942e37a0595bf5dd86
Your Name <you@example.com>
2022-07-20 16:48:59 +0430

    test javascript

--- woocommerce/cart/proceed-to-checkout-button.php
+++ woocommerce/cart/proceed-to-checkout-button.php
@@ -22,6 +22,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class="checkout-button button alt wc-forward">
+<a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class="checkout-button alt wc-forward btn btn-success">
 	<?php esc_html_e( 'Proceed to checkout', 'woocommerce' ); ?>
 </a>
