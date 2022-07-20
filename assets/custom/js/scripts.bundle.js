96eb5ad2e5248cff2d895d744964655d59a7af55
Your Name <you@example.com>
2022-07-20 11:46:33 +0430

    test javascript

--- assets/custom/js/scripts.bundle.js
+++ assets/custom/js/scripts.bundle.js
@@ -415,6 +415,15 @@ jQuery(function () {
     $('.shop_table').find('.quantity').append('<button type="button" class="btn btn-icon btn-active-color-gray-700 position-absolute translate-middle-y top-50 end-0" data-kt-dialer-control="increase"><span class="svg-icon svg-icon-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5" fill="currentColor"></rect><rect x="10.8891" y="17.8033" width="12" height="2" rx="1" transform="rotate(-90 10.8891 17.8033)" fill="currentColor"></rect><rect x="6.01041" y="10.9247" width="12" height="2" rx="1" fill="currentColor"></rect></svg></span></button>');
     $('.shop_table').find('.quantity').find('input').attr('type', 'text').addClass('form-control form-control-solid border-0 text-center ss02 w-100px').attr('readonly', true).attr('data-kt-dialer-control', 'input').removeAttr('inputmode').removeAttr('autocomplete').removeAttr('size').removeAttr('title');
     $('.shop_table').find('.quantity').addClass('position-relative w-100px d-inline-block');
+    var addToCartMin = parseInt($('.shop_table').find('.quantity').find('input').attr('min'));
+    var addToCartMax = parseInt($('.shop_table').find('.quantity').find('input').attr('max'));
+    var addToCartStep = parseInt($('.shop_table').find('.quantity').find('input').attr('step'));
+    var addToCartDialerElement = document.querySelector(".quantity");
+    var addToCartDialerObject = new KTDialer(addToCartDialerElement, {
+        min: addToCartMin,
+        max: addToCartMax,
+        step: addToCartStep
+    })
 
 
 
