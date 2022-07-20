0fbd69a5f80761225503dcdf1b0edf2475308d30
Your Name <you@example.com>
2022-07-20 21:49:49 +0430

    cart edit

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -61,7 +61,7 @@ do_action('woocommerce_before_cart'); ?>
 						<tr>
 							<th class="product-remove">&nbsp;</th>
 							<th class="product-thumbnail">&nbsp;</th>
-							<th class="product-name"><?php esc_html_e('Product', 'woocommerce'); ?></th>
+							<th class="product-name"><b><?php esc_html_e('Product', 'woocommerce'); ?></b></th>
 							<th class="product-price"><?php esc_html_e('Price', 'woocommerce'); ?></th>
 							<th class="product-quantity"><?php esc_html_e('Quantity', 'woocommerce'); ?></th>
 							<th class="product-subtotal"><?php esc_html_e('Subtotal', 'woocommerce'); ?></th>
