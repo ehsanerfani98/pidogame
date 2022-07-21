ffae7b3bca957256986e69319de9f976df4a701b
Your Name <you@example.com>
2022-07-21 13:24:35 +0430

    edit script

--- footer.php
+++ footer.php
@@ -7,8 +7,6 @@
 <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/custom/fslightbox/fslightbox.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/js/scripts.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/custom/js/scripts.bundle.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/custom/Iris/src/iris.js"></script>
 
 
