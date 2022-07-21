709cb79179934609298c7fbb8b8ef02e6e3935fe
Your Name <you@example.com>
2022-07-21 13:08:53 +0430

    edit script

--- footer.php
+++ footer.php
@@ -3,8 +3,7 @@
     var templateDirectory = "<?php echo get_template_directory_uri() ?>";
 </script>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="<?php echo get_template_directory_uri() ?>/assets/plugins/global/plugins.bundle.js"></script>
+<!-- <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/global/plugins.bundle.js"></script> -->
 <script src="<?php echo get_template_directory_uri() ?>/assets/plugins/custom/fslightbox/fslightbox.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/js/scripts.bundle.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/assets/custom/js/scripts.bundle.js"></script>
