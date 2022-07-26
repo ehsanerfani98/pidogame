fa2361806e74d1019638d54680c80f7703f0cec6
Your Name <you@example.com>
2022-07-26 20:17:12 +0430

    edit archive

--- functions.php
+++ functions.php
@@ -1056,15 +1056,19 @@ function custom_price_format( $price, $product ) {
 
     // Formatting the price
     if ( $regular_price !== $sale_price && $product->is_on_sale()) {
+
+		if(empty($regular_price)){
+			$price = '<div class=" mx-2 fs-5 px-4 py-2">' . wc_price($sale_price) . '</div>';
+			return $price;
+		}
+
         // Percentage calculation and text
         $percentage = round( ( $regular_price - $sale_price ) / $regular_price * 100 ).'%';
         $percentage_txt = __(' Save', 'woocommerce' ).' '.$percentage;
 
         // $price = '<del class="badge badge-danger">' . wc_price($regular_price) . '</del> <ins>' . wc_price($sale_price) . $percentage_txt . '</ins>';
         $price = '<div class=" mx-2 fs-5 px-4 py-2"><del>' . wc_price($regular_price) . ' </del>  </div><div class="badge badge-success mx-2 fs-5 px-4 py-2">' . wc_price($sale_price) . '</div>';
-    }elseif(empty($regular_price)){
-        $price = '<div class=" mx-2 fs-5 px-4 py-2">' . wc_price($sale_price) . '</div>';
-	}
+    }
 	else{
         $price = '<div class=" mx-2 fs-5 px-4 py-2">' . wc_price($regular_price) . '</div><div class=" mx-2 fs-5 px-4 py-2">' . wc_price($sale_price) . '</div>';
 	}
