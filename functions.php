009d36c31d74c55adef80453b54dcf91e86dbd01
Your Name <you@example.com>
2022-07-26 20:20:14 +0430

    edit archive

--- functions.php
+++ functions.php
@@ -1071,7 +1071,7 @@ function custom_price_format( $price, $product ) {
 			$price = '<div class=" mx-2 fs-5 px-4 py-2">' . wc_price($sale_price) . '</div>';
 			return $price;
 		}
-        $price = '<div class=" mx-2 fs-5 px-4 py-2">' . wc_price($regular_price) . '</div><div class=" mx-2 fs-5 px-4 py-2">' . wc_price($sale_price) . '</div>';
+        // $price = '<div class=" mx-2 fs-5 px-4 py-2">' . wc_price($regular_price) . '</div><div class=" mx-2 fs-5 px-4 py-2">' . wc_price($sale_price) . '</div>';
 	}
     return $price;
 }
