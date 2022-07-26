a5e9df9f47f67834f706539a1738cb153fd2311d
Your Name <you@example.com>
2022-07-26 14:01:06 +0430

    edit archive

--- woocommerce/loop/pagination.php
+++ woocommerce/loop/pagination.php
@@ -80,5 +80,5 @@ if ($total <= 1) {
 
 <script>
 	jQuery('a.page-numbers').addClass('btn btn-primary');
-	jQuery('span.page-numbers.current').addClass('btn btn-info').removeClass('page-numbers').removeClass('current');
+	jQuery('span.page-numbers.current').addClass('btn bg-primary').removeClass('page-numbers').removeClass('current');
 </script>
