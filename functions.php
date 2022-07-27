dd52aed43a2906957784bb29e4293ddf371abbc9
Your Name <you@example.com>
2022-07-27 21:04:33 +0430

    edit extra option plugin

--- functions.php
+++ functions.php
@@ -74,7 +74,7 @@ function fx_check($pid, $vid)
 
 							$product = wc_get_product($not_variation_id);
 
-							if ($product->is_type('simple')) {
+							if ($product->is_type('variation')) {
 								var_dump('simple product');
 							} elseif ($product->is_type('variable')) {
 								var_dump('variable product');
