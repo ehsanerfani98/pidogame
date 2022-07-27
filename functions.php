734312dacc359cfe893fdf84c7e53db2b6167bb8
Your Name <you@example.com>
2022-07-27 21:27:09 +0430

    edit extra option plugin

--- functions.php
+++ functions.php
@@ -59,6 +59,7 @@ function fx_check($pid, $vid)
 
 
 			foreach ($extra_fields as $item) {
+				var_dump($item['disable_all_rule_products']);
 				if ($item['disable_all_rule_products']) {
 					foreach ($item['show_products_inside_fields'] as $show_product) {
 						$variation_id = $show_product->ID;
