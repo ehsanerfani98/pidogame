276f6bb3aed3f5ec1634636d2ed0897022176fc4
Your Name <you@example.com>
2022-07-28 10:23:04 +0430

    edit extra option

--- templates/product/game/header.php
+++ templates/product/game/header.php
@@ -369,33 +369,34 @@ foreach (explode(',', $product->get_attribute('pa_device')) as $name) {
                                                                     </form>
                                                                 </div>
 
-                                                                <div class="d-flex align-items-center justify-content-center mt-4 mt-md-2 mt-xl-0">
-                                                                    <div class="d-flex flex-column justify-content-center align-items-center">
-                                                                    <?php if ($variationProduct->is_on_sale()) : ?>
-                                                                        <span class="text-muted fw-bold fs-4 me-3 mt-1 ss02 text-decoration-line-through"><?php echo number_format($variationProduct->regular_price) ?></span>
-                                                                        <span class="text-primary fw-boldest fs-4 ss02"><?php echo number_format($variationProduct->sale_price) ?></span>
-                                                                        <span class="fw-bold fs-4 text-gray-600 me-1 ms-2"><?php echo get_woocommerce_currency_symbol() ?></span>
-                                                                        <?php $percentage = intval((($variationProduct->get_regular_price() - $variationProduct->get_sale_price()) / $variationProduct->get_regular_price()) * 100) ?>
-                                                                        <span class="badge badge-lg badge-success align-self-center px-2 ms-3 ss02"><?php echo $percentage ?>% تخفیف</span>
-                                                                        <?php $salesPriceTo = null;
-                                                                        $salesPriceTo = get_post_meta($variationProduct->get_id(), '_sale_price_dates_to', true);
-                                                                        if ($salesPriceTo) :
-                                                                            $salesPriceDateTo = date("Y-m-j H:i:s", $salesPriceTo);
-                                                                            $now = new DateTime();
-                                                                            $futureDate = new DateTime($salesPriceDateTo);
-                                                                            $interval = $futureDate->diff($now);
-                                                                            $diff = $interval->format("%a روز و %h ساعت و %i دقیقه") ?>
-                                                                            <span class="d-block d-xl-none position-absolute top-0 start-50 translate-middle badge badge-danger ss02"><?php echo $diff ?> باقی مانده</span>
-                                                                            <span class="d-none d-xl-block position-absolute top-0 start-75 translate-middle-y badge badge-danger ss02"><?php echo $diff ?> باقی مانده</span>
+                                                                <div class="d-flex flex-column justify-content-center align-items-center">
+                                                                    <div class="d-flex align-items-center justify-content-center mt-4 mt-md-2 mt-xl-0">
+                                                                        <?php if ($variationProduct->is_on_sale()) : ?>
+                                                                            <span class="text-muted fw-bold fs-4 me-3 mt-1 ss02 text-decoration-line-through"><?php echo number_format($variationProduct->regular_price) ?></span>
+                                                                            <span class="text-primary fw-boldest fs-4 ss02"><?php echo number_format($variationProduct->sale_price) ?></span>
+                                                                            <span class="fw-bold fs-4 text-gray-600 me-1 ms-2"><?php echo get_woocommerce_currency_symbol() ?></span>
+                                                                            <?php $percentage = intval((($variationProduct->get_regular_price() - $variationProduct->get_sale_price()) / $variationProduct->get_regular_price()) * 100) ?>
+                                                                            <span class="badge badge-lg badge-success align-self-center px-2 ms-3 ss02"><?php echo $percentage ?>% تخفیف</span>
+                                                                            <?php $salesPriceTo = null;
+                                                                            $salesPriceTo = get_post_meta($variationProduct->get_id(), '_sale_price_dates_to', true);
+                                                                            if ($salesPriceTo) :
+                                                                                $salesPriceDateTo = date("Y-m-j H:i:s", $salesPriceTo);
+                                                                                $now = new DateTime();
+                                                                                $futureDate = new DateTime($salesPriceDateTo);
+                                                                                $interval = $futureDate->diff($now);
+                                                                                $diff = $interval->format("%a روز و %h ساعت و %i دقیقه") ?>
+                                                                                <span class="d-block d-xl-none position-absolute top-0 start-50 translate-middle badge badge-danger ss02"><?php echo $diff ?> باقی مانده</span>
+                                                                                <span class="d-none d-xl-block position-absolute top-0 start-75 translate-middle-y badge badge-danger ss02"><?php echo $diff ?> باقی مانده</span>
+                                                                            <?php endif ?>
+                                                                        <?php else : ?>
+                                                                            <span class="text-primary fw-boldest fs-4 ss02"><?php echo number_format($variationProduct->regular_price) ?></span>
+                                                                            <span class="fw-bold fs-4 text-gray-600 me-1 ms-2"><?php echo get_woocommerce_currency_symbol() ?></span>
                                                                         <?php endif ?>
-                                                                    <?php else : ?>
-                                                                        <span class="text-primary fw-boldest fs-4 ss02"><?php echo number_format($variationProduct->regular_price) ?></span>
-                                                                        <span class="fw-bold fs-4 text-gray-600 me-1 ms-2"><?php echo get_woocommerce_currency_symbol() ?></span>
-                                                                    <?php endif ?>
+                                                                    </div>
+
                                                                     <div class="d-flex justify-content-between align-items-center mt-4">
                                                                         <span class="badge badge-secondary p-2"><?= $title ?></span>
                                                                         <span class="badge badge-secondary p-2"><?= $deliveryTime ?></span>
-                                                                    </div>
                                                                     </div>
                                                                 </div>
 
