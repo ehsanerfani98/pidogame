68831af863bb0abf39a6a5bd79d8ff5eb64a252f
Your Name <you@example.com>
2022-07-30 13:25:09 +0430

    edit extra option plugin new

--- functions.php
+++ functions.php
@@ -42,7 +42,7 @@ function fx_check($pid, $vid)
 
 			foreach ($display_rules as $product_id) {
 
-				$product = new WC_Product_Variable(3850);
+				$product = wc_get_product((object)3849);
 			dd($product->get_children());
 
 				if (!$product->is_type('variable')) {
