41bef845d6a023d3577a15d2ea12df2337b0c20f
Your Name <you@example.com>
2022-07-30 14:28:46 +0430

    edit extra option plugin new

--- templates/product/game/header.php
+++ templates/product/game/header.php
@@ -224,7 +224,6 @@ foreach (explode(',', $product->get_attribute('pa_device')) as $name) {
 
                                                                         <div class="d-flex flex-column">
                                                                             <?php $plswb_fields = fx_check(get_the_ID(), $variation['variation_id']);
-                                                                            dd($plswb_fields);
                                                                             if (count($plswb_fields) > 0) :
                                                                             ?>
                                                                                 <?php foreach ($plswb_fields as $key => $item) : ?>
