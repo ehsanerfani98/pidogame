d9577d4e9b093e8cca549edb4697a9526f0c7e48
Your Name <you@example.com>
2022-07-30 14:12:46 +0430

    edit extra option plugin new

--- assets/js/jq.multiinput.js
+++ assets/js/jq.multiinput.js
@@ -20,7 +20,11 @@ jQuery(document).ready(function ($) {
     });
 
     $('.products_rules_db').select2({
-
+        language: {
+            searching: function() {
+                return "در حال جستجو...";
+            }
+        },
         placeholder: "محصولات مورد نظر خود را انتخاب کنید.",
         ajax: {
             url: rankMath.ajaxurl,
