55ceeb135e51a495a11c0aa2a9186917f72d63ea
Your Name <you@example.com>
2022-07-30 15:52:10 +0430

    edit extra option plugin new

--- functions.php
+++ functions.php
@@ -1093,7 +1093,7 @@ function fx_check($pid, $vid)
 							$not_variation_id = $not_show_product_id;
 
 							$product = wc_get_product($not_variation_id);
-
+dd($product);
 							if ($product->is_type('variation')) {
 								$pos = array_search($not_variation_id, $variation_ids);
 								if ($pos !== false) {
