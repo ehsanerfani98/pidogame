d77be35787dbfa5b697f4b3a582ff5ed32a3c3ff
Your Name <you@example.com>
2022-07-30 13:03:27 +0430

    edit extra option plugin new

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ function fx_check($pid, $vid)
 			}
 							$variations = wc_get_product(3849);
 
-			dd($variations);
+			dd($variations->get_children());
 
 
 			foreach ($extra_fields as $item) {
