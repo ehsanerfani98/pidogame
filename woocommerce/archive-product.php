94f765ad7df4ff732a85c7de0b2bc335d2ef3272
Your Name <you@example.com>
2022-08-08 20:51:07 +0430

    user panel

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -108,7 +108,7 @@ if (woocommerce_product_loop()) {
  *
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
-do_action('woocommerce_after_main_content');
+// do_action('woocommerce_after_main_content');
 
 /**
  * Hook: woocommerce_sidebar.
