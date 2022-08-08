6c7286427b58f49894089a6b4ddc15c610f590ac
Your Name <you@example.com>
2022-08-09 02:51:21 +0430

    user panel

--- templates/page/header/toolbar/user.php
+++ templates/page/header/toolbar/user.php
@@ -17,9 +17,11 @@ if (is_user_logged_in()) :
                         <div class="fw-bolder d-flex align-items-center fs-5">
                             <?php echo $currentUser->display_name ?>
                             <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2 ss02"><?php
-                            if(in_array(do_shortcode( '[avans-user-score]', ignore_html ),get_option( 'avans_plugin_setting' )['avans_user_levels'])){
-                                echo get_option( 'avans_plugin_setting' )['avans_user_levels']['level'];
-                            }
+                            foreach (get_option( 'avans_plugin_setting' )['avans_user_levels'] as $key => $value) {
+                                if($value['min_score'] == do_shortcode( '[avans-user-score]', ignore_html )){
+                                    echo $value['level'];
+                                }
+                                }
                              ?></span>
                         </div>
                         <a class="fw-bold text-muted fs-7"><?php echo $currentUser->user_email ?></a>
