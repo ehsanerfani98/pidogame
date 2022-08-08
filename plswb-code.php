2cf1b3e2010efb89a844aaaa1ce381a08ae79aff
Your Name <you@example.com>
2022-08-08 20:54:43 +0430

    user panel

--- plswb-code.php
+++ plswb-code.php
@@ -535,8 +535,6 @@ add_filter( 'woocommerce_account_orders_columns', 'filter_woocommerce_account_or
 add_action('woocommerce_archive_description', 'custom_archive_description', 2);
 
 function custom_archive_description(){ 
-  if ( is_product_category() ){
     remove_action('woocommerce_archive_description', 'woocommerce_taxonomy_archive_description', 10 );
     add_action('woocommerce_after_main_content', 'woocommerce_taxonomy_archive_description', 5 );
-  }
 }
