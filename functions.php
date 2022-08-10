9d59a3967533954aea74533328879cd8a099d48b
Your Name <you@example.com>
2022-08-10 16:36:50 +0430

    comments

--- functions.php
+++ functions.php
@@ -1110,8 +1110,7 @@ function woo_general_init()
 						if ($field['disable_org_show_products_rules']) {
 							foreach ($field['inside_show_products_rules'] as $inside_product_id) {
 								$product = wc_get_product($inside_product_id);
-																if ($product->is_type('simple')) {
-																}
+																var_dump($product->is_type('simple'));
 								// if ($product->is_type('simple')) {
 								// 	$inside_variation_ids[] = $inside_product_id;
 								// } else {
