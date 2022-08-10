61119cff1a533df5631c4f35cf6e9508beb0b705
Your Name <you@example.com>
2022-08-11 01:36:28 +0430

    comments

--- plswb-code.php
+++ plswb-code.php
@@ -592,7 +592,7 @@ add_action('wp_ajax_nopriv_search_data_product', 'search_data_product');
 function search_data_product()
 {
     $options = get_option('pidogame_framework')['search-product-likes_fields'];
-    $the_query = new WP_Query(array('posts_per_page' => -1, 'search_prod_title' => esc_attr($_POST['keyword']), 'post_type' => 'product'));
+    $the_query = new WP_Query(array('posts_per_page' => -1, 's' => esc_attr($_POST['keyword']), 'post_type' => 'product'));
     if ($the_query->have_posts()) :
         while ($the_query->have_posts()) : $the_query->the_post(); ?>
             <a href="<?php echo esc_url(post_permalink()); ?>" class="d-flex text-dark text-hover-primary align-items-center mb-5">
