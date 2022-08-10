201ee74182328a9c1d60165e4c4207c3f212bdb7
Your Name <you@example.com>
2022-08-10 16:14:20 +0430

    comments

--- functions.php
+++ functions.php
@@ -1082,9 +1082,143 @@ function woo_general_init()
 		$fields_plswb = new WP_Query($arg);
 
 		if ($fields_plswb->have_posts()) {
-		
+			while ($fields_plswb->have_posts()) {
+				$fields_plswb->the_post();
+				$display_rules = get_post_meta(get_the_ID(), "all_products_show_rules", true);
+				$extra_fields = get_post_meta(get_the_ID(), "plswb_fields", true);
+				if (!empty($display_rules)) {
+					foreach ($display_rules as $product_id) {
+						$product = wc_get_product($product_id);
+						if ($product->is_type('simple')) {
+							$all_org_variation_ids[] = $product_id;
+						} else {
+							if ($product->is_type('variation')) {
+								$all_org_variation_ids[] = $product_id;
+							} else {
+								$variations = new WC_Product_Variable($product_id);
+								foreach ($variations->get_children() as  $v_id) {
+									$all_org_variation_ids[] = $v_id;
+								}
+							}
+						}
+					}
+				}
+
+
+				$all_org_variation_ids = array_unique($all_org_variation_ids);
+				if (!empty($extra_fields)) {
+					foreach ($extra_fields as $field) {
+						if ($field['disable_org_show_products_rules']) {
+
+							foreach ($field['inside_show_products_rules'] as $inside_product_id) {
+								$product = wc_get_product($inside_product_id);
+								if ($product->is_type('simple')) {
+									$inside_variation_ids[] = $inside_product_id;
+								} else {
+									if ($product->is_type('variation')) {
+										$inside_variation_ids[] = $inside_product_id;
+									} else {
+										$inside_variations = new WC_Product_Variable($inside_product_id);
+										foreach ($inside_variations->get_children() as  $inside_v_id) {
+											$inside_variation_ids[] = $inside_v_id;
+										}
+									}
+								}
+							}
+
+							$inside_variation_ids = array_unique($inside_variation_ids);
+
+							if (count($field['not_show_products_rules']) > 0) {
+								if (!empty($field['not_show_products_rules'])) {
+									foreach ($field['not_show_products_rules'] as $not_show_product_id) {
+										$product = wc_get_product($not_show_product_id);
+										if ($product->is_type('simple')) {
+											$not_show_variation_ids[] = $not_show_product_id;
+										} else {
+											if ($product->is_type('variation')) {
+												$not_show_variation_ids[] = $not_show_product_id;
+											} else {
+												$not_show_variations = new WC_Product_Variable($not_show_product_id);
+												if (!empty($not_show_variations->get_children())) {
+													foreach ($not_show_variations->get_children() as  $not_show_v_id) {
+														$not_show_variation_ids[] = $not_show_v_id;
+													}
+												}
+											}
+										}
+									}
+								}
+								$not_show_variation_ids = array_unique($not_show_variation_ids);
+								$display_rules_ids = array_diff($inside_variation_ids, $not_show_variation_ids);
+								unset($not_show_variation_ids);
+							} else {
+								$display_rules_ids = $inside_variation_ids;
+								unset($inside_variation_ids);
+							}
+
+							if (is_null($vid)) {
+								if (in_array($pid, $display_rules_ids)) {
+									$created_fields[] = $field;
+								}
+							} else {
+								if (in_array($vid, $display_rules_ids)) {
+									$created_fields[] = $field;
+								}
+							}
+						} else {
+							if (count($field['not_show_products_rules']) > 0) {
+								if (!empty($field['not_show_products_rules'])) {
+									foreach ($field['not_show_products_rules'] as $not_show_product_id) {
+										$product = wc_get_product($not_show_product_id);
+										if ($product->is_type('simple')) {
+											$not_show_variation_ids[] = $not_show_product_id;
+										} else {
+											if ($product->is_type('variation')) {
+												$not_show_variation_ids[] = $not_show_product_id;
+											} else {
+												$not_show_variations = new WC_Product_Variable($not_show_product_id);
+												foreach ($not_show_variations->get_children() as  $not_show_v_id) {
+													$not_show_variation_ids[] = $not_show_v_id;
+												}
+											}
+										}
+									}
+								}
+
+								$not_show_variation_ids = array_unique($not_show_variation_ids);
+								$display_rules_ids = array_diff($all_org_variation_ids, $not_show_variation_ids);
+								unset($not_show_variation_ids);
+
+								if (is_null($vid)) {
+									if (in_array($pid, $display_rules_ids)) {
+										$created_fields[] = $field;
+									}
+								} else {
+									if (in_array($vid, $display_rules_ids)) {
+										$created_fields[] = $field;
+									}
+								}
+							} else {
+
+								if (is_null($vid)) {
+									if (in_array($pid, $all_org_variation_ids)) {
+										$created_fields[] = $field;
+									}
+								} else {
+									if (in_array($vid, $all_org_variation_ids)) {
+										$created_fields[] = $field;
+									}
+								}
+							}
+						}
+					}
+				}
+
+				unset($all_org_variation_ids);
+				unset($inside_variation_ids);
+			}
+			wp_reset_postdata();
 		}
-		dd('$l');
 
 		return $created_fields;
 	}
