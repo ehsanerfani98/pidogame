5457c56e5ef58e1485caad835ab1bce577ef1fb2
Your Name <you@example.com>
2022-08-10 15:41:20 +0430

    comments

--- functions.php
+++ functions.php
@@ -1202,13 +1202,12 @@ function woo_general_init()
 				}
 				unset($all_org_variation_ids);
 				unset($inside_variation_ids);
-		dd($created_fields);
+				return $created_fields;
 
 			}
 			wp_reset_postdata();
 		}
 
-		return $created_fields;
 	}
 }
 
