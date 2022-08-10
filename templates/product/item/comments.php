f2ad5a28bb4a93764f54f76ff7044c887a45e3cc
Your Name <you@example.com>
2022-08-10 21:23:41 +0430

    comments

--- templates/product/item/comments.php
+++ templates/product/item/comments.php
@@ -54,7 +54,7 @@ function echoFirstLevelComment($comment)
                     </div>
                 </div>
                 <div class="d-flex align-items-center py-1">
-                    <div class="rating me-2">
+                    <!-- <div class="rating me-2">
                         <?php $rating = get_comment_meta($comment->comment_ID, 'rating', true) ?>
                         <?php if ($rating > 0) : ?>
                             <?php for ($i = 1; $i <= 5; $i++) : ?>
@@ -67,7 +67,17 @@ function echoFirstLevelComment($comment)
                                 </div>
                             <?php endfor ?>
                         <?php endif ?>
-                    </div>
+                    </div> -->
+                    <span class="d-flex align-items-center mb-2">
+                            <span onclick="setLikeComment(this, <?= $comment->comment_ID ?>)" style="cursor: pointer;" class="svg-icon <?= isset($_COOKIE[$comment->comment_ID]) ? 'svg-icon-danger' : 'svg-icon-muted' ?>"><svg xmlns="http://www.w3.org/2000/svg" class="h-25px w-25px" viewBox="0 0 24 24" fill="none">
+                                    <path d="M18.3721 4.65439C17.6415 4.23815 16.8052 4 15.9142 4C14.3444 4 12.9339 4.73924 12.003 5.89633C11.0657 4.73913 9.66 4 8.08626 4C7.19611 4 6.35789 4.23746 5.62804 4.65439C4.06148 5.54462 3 7.26056 3 9.24232C3 9.81001 3.08941 10.3491 3.25153 10.8593C4.12155 14.9013 9.69287 20 12.0034 20C14.2502 20 19.875 14.9013 20.7488 10.8593C20.9109 10.3491 21 9.81001 21 9.24232C21.0007 7.26056 19.9383 5.54462 18.3721 4.65439Z" fill="black" />
+                                </svg></span>
+                            <span class="text-muted mr-1" id="like-total">
+                                <?php if (!empty(get_comment_meta($comment->comment_ID, 'total_like', true))) : ?>
+                                    <?= "(" . get_comment_meta($comment->comment_ID, 'total_like', true) . ")" ?>
+                                <?php endif; ?>
+                            </span>
+                        </span>
                     <a data-reply-id="<?php echo $comment->comment_ID ?>" href="#comment" class="reply-comment btn btn-sm btn-flex btn-color-gray-500 btn-active-light me-1" data-kt-scroll-toggle>پاسخ</a>
                 </div>
             </div>
@@ -106,7 +116,7 @@ function echoChildrenComment($comment)
                         </div>
                     </div>
                     <div class="d-flex align-items-center py-1">
-                        <div class="rating me-2">
+                        <!-- <div class="rating me-2">
                             <?php $rating = get_comment_meta($comment->comment_ID, 'rating', true) ?>
                             <?php if ($rating > 0) : ?>
                                 <?php for ($i = 1; $i <= 5; $i++) : ?>
@@ -119,7 +129,17 @@ function echoChildrenComment($comment)
                                     </div>
                                 <?php endfor ?>
                             <?php endif ?>
-                        </div>
+                        </div> -->
+                        <span class="d-flex align-items-center mb-2">
+                            <span onclick="setLikeComment(this, <?= $comment->comment_ID ?>)" style="cursor: pointer;" class="svg-icon <?= isset($_COOKIE[$comment->comment_ID]) ? 'svg-icon-danger' : 'svg-icon-muted' ?>"><svg xmlns="http://www.w3.org/2000/svg" class="h-25px w-25px" viewBox="0 0 24 24" fill="none">
+                                    <path d="M18.3721 4.65439C17.6415 4.23815 16.8052 4 15.9142 4C14.3444 4 12.9339 4.73924 12.003 5.89633C11.0657 4.73913 9.66 4 8.08626 4C7.19611 4 6.35789 4.23746 5.62804 4.65439C4.06148 5.54462 3 7.26056 3 9.24232C3 9.81001 3.08941 10.3491 3.25153 10.8593C4.12155 14.9013 9.69287 20 12.0034 20C14.2502 20 19.875 14.9013 20.7488 10.8593C20.9109 10.3491 21 9.81001 21 9.24232C21.0007 7.26056 19.9383 5.54462 18.3721 4.65439Z" fill="black" />
+                                </svg></span>
+                            <span class="text-muted mr-1" id="like-total">
+                                <?php if (!empty(get_comment_meta($comment->comment_ID, 'total_like', true))) : ?>
+                                    <?= "(" . get_comment_meta($comment->comment_ID, 'total_like', true) . ")" ?>
+                                <?php endif; ?>
+                            </span>
+                        </span>
                         <a data-reply-id="<?php echo $comment->comment_ID ?>" href="#comment" class="reply-comment btn btn-sm btn-flex btn-color-gray-500 btn-active-light me-1" data-kt-scroll-toggle>پاسخ</a>
                     </div>
                 </div>
