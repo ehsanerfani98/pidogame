53d510532150fa92174c5a688ef4c5e72b2a32c9
Your Name <you@example.com>
2022-08-11 22:05:46 +0430

    comments

--- search.php
+++ search.php
@@ -134,9 +134,12 @@ get_header();
                             </ul>
                         </div>
                         <?php
+                        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
                         $args = array(
                             'post_type'        => 'post',
                             'posts_per_page'   => 12,
+                            'paged' => $paged,
                             'status'         => 'publish',
                         );
                         $query = new WP_Query($args);
@@ -171,8 +174,30 @@ get_header();
 
 
                                 </div>
-                        <?php
+                            <?php
                             endwhile;
+                            ?>
+                            <div class="pagination">
+                                <?php
+                                echo paginate_links(array(
+                                    'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+                                    'total'        => $query->max_num_pages,
+                                    'current'      => max(1, get_query_var('paged')),
+                                    'format'       => '?paged=%#%',
+                                    'show_all'     => false,
+                                    'type'         => 'plain',
+                                    'end_size'     => 2,
+                                    'mid_size'     => 1,
+                                    'prev_next'    => true,
+                                    'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
+                                    'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
+                                    'add_args'     => false,
+                                    'add_fragment' => '',
+                                ));
+                                ?>
+                            </div>
+                        <?php
+                            wp_reset_postdata();
                         endif ?>
                     </div>
                 </div>
