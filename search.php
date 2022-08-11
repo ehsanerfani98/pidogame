c084e5c5ce266c89e80d6878e6cd701fa5e1d2fc
Your Name <you@example.com>
2022-08-11 20:44:04 +0430

    comments

--- search.php
+++ search.php
@@ -9,15 +9,15 @@ get_header();
             <div id="kt_content_container" class="d-flex flex-column-fluid align-items-start container-xxl">
                 <?php get_template_part('templates/page/aside/aside') ?>
                 <div class="content flex-row-fluid" id="kt_content">
+                    <div class="row">
                     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
                             <div class="col-md-3">
                                 <?php the_title() ?>
                             </div>
-
                     <?php
                         endwhile;
                     endif ?>
+                    </div>
                 </div>
             </div>
             <?php get_template_part('templates/page/footer/footer') ?>
