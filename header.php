6d86751b11a69972a47582c9ad09ce1e722ccbee
Your Name <you@example.com>
2022-08-11 23:12:36 +0430

    comments

--- header.php
+++ header.php
@@ -51,7 +51,7 @@ $data_alert = get_option('pidogame_framework');
             <div class="d-flex flex-column text-light pe-0 pe-sm-10">
                 <h4 class="mb-2 text-light"></h4>
 
-                <h4 class="text-gray-100">
+                <h4 class="text-gray-100 p-1">
                     <?= $data_alert['at_title'] ?>
                 </h4>
             </div>
