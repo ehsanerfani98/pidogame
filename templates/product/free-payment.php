00e20f355c17459feead322ce18b6d36cf09929c
Your Name <you@example.com>
2022-08-11 20:07:03 +0430

    comments

--- templates/product/free-payment.php
+++ templates/product/free-payment.php
@@ -16,7 +16,7 @@
 
                         ?>
                     </div>
-                    <div class="card-footer border border-primary border-dashed pb-0">
+                    <div class="card-footer bg-secondary border border-primary border-dashed pb-0">
                         <input type="hidden" id="pid" value="<?php the_ID() ?>">
                         <div class="row">
                             <div class="col-lg-4">
