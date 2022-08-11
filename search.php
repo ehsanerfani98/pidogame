51877ef4c1a322e59335f5af625428bd0f44a4a1
Your Name <you@example.com>
2022-08-11 21:48:24 +0430

    comments

--- search.php
+++ search.php
@@ -130,7 +130,7 @@ get_header();
                         <?php
                         $args = array(
                             'post_type'        => 'post',
-                            'posts_per_page'   => -1,
+                            'posts_per_page'   => 12,
                             'status'         => 'publish',
                         );
                         $query = new WP_Query($args);
