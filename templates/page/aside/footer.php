3743e2bdb80f36b816bf96cef8e517d57648e047
Your Name <you@example.com>
2022-08-13 19:43:09 +0430

    check out mobile field

--- templates/page/aside/footer.php
+++ templates/page/aside/footer.php
@@ -3,7 +3,7 @@
     <div class="d-flex flex-center w-100 scroll-px" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-dismiss="click" title="شبکه های اجتماعی">
         <button type="button" class="btn btn-icon btn-custom w-50px h-50px" data-kt-menu-trigger="click" data-kt-menu-overflow="true" data-kt-menu-placement="top-start">
             <!--begin::Svg Icon-->
-            <span class="svg-icon svg-icon-2 svg-icon-lg-1">
+            <span class="svg-icon svg-icon-2 svg-icon-lg-1" style="color: white;">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
                     <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
                 </svg>
