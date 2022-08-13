e99088fbbc3d9abdaf9b034c4cc6c7a5ce4acc32
Your Name <you@example.com>
2022-08-14 03:09:21 +0430

    alert

--- templates/page/header/toolbar/notifications.php
+++ templates/page/header/toolbar/notifications.php
@@ -106,7 +106,12 @@ if ($options['opt-header-notifications-switcher']) :
                         <div class="scroll-y mh-325px my-5 px-8">
                             <?php 
                             foreach (get_all_order() as $order_id) {
-                                $notes[] = get_private_order_notes($order_id);
+                                $notes[] = wc_get_order_notes([
+                                    'order_id' => $order_id,
+                                    'type' => 'customer',
+                                 ]);
+                                 
+                                // $notes[] = get_private_order_notes($order_id);
                             } 
                             var_dump($notes);
                             ?>
