82bc41cff53d1972b88b452b752622c786808a3b
Your Name <you@example.com>
2022-08-14 02:39:04 +0430

    alert

--- templates/page/header/toolbar/notifications.php
+++ templates/page/header/toolbar/notifications.php
@@ -131,12 +131,14 @@ if ($options['opt-header-notifications-switcher']) :
                             <!--end::Label-->
                         </div>
                     </div>
-                    <div class="d-flex flex-column px-9">
-                        <div class="pt-10 pb-0">
-                            <h3 class="text-dark text-center fw-bolder">f</h3>
-                            <div class="text-center text-gray-600 fw-bold pt-1">e</div>
-                        </div>
-                      
+                    <div class="py-3 text-center border-top">
+                        <a target="" href="http://test.pidogame.com/go" class="btn btn-color-gray-600 btn-active-color-primary">مشاهده همه <span class="svg-icon svg-icon-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <rect opacity="0.5" x="6" y="11" width="13" height="2" rx="1" fill="currentColor"></rect>
+                                    <path d="M8.56569 11.4343L12.75 7.25C13.1642 6.83579 13.1642 6.16421 12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75L5.70711 11.2929C5.31658 11.6834 5.31658 12.3166 5.70711 12.7071L11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25C13.1642 17.8358 13.1642 17.1642 12.75 16.75L8.56569 12.5657C8.25327 12.2533 8.25327 11.7467 8.56569 11.4343Z" fill="currentColor"></path>
+                                </svg>
+                            </span>
+                        </a>
                     </div>
                 </div>
             </div>
