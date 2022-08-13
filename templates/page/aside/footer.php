98f91cea83ca5bbd8db0a6777f96a30ab84393fc
Your Name <you@example.com>
2022-08-13 19:01:05 +0430

    check out mobile field

--- templates/page/aside/footer.php
+++ templates/page/aside/footer.php
@@ -31,12 +31,15 @@
                 if ($item->menu_item_parent == 0) :
             ?>
                     <div class="menu-item px-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <div class="d-flex align-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5" fill="black"/>
 <rect x="10.8891" y="17.8033" width="12" height="2" rx="1" transform="rotate(-90 10.8891 17.8033)" fill="black"/>
 <rect x="6.01041" y="10.9247" width="12" height="2" rx="1" fill="black"/>
 </svg>
                         <a href="<?= $item->url ?>" class="menu-link px-3"><?= $item->title ?></a>
+
+                        </div>
                     </div>
                 <?php endif; ?>
             <?php endforeach; ?>
