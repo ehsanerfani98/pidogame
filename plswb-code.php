5520eb7982563410e490c2cf1f38714de7d23db6
Your Name <you@example.com>
2022-08-14 03:50:55 +0430

    alert

--- plswb-code.php
+++ plswb-code.php
@@ -1116,7 +1116,6 @@ function get_private_order_notes($order_id)
     ");
 
     foreach ($results as $note) {
-        var_dump($note);
         $order_note[]  = array(
             'note_id'      => $note->comment_ID,
             'note_date'    => $note->comment_date,
