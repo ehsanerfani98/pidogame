69b18078cc3b6999add8b151b8caa64a9201342b
Your Name <you@example.com>
2022-08-13 19:04:55 +0430

    check out mobile field

--- templates/page/aside/footer.php
+++ templates/page/aside/footer.php
@@ -33,12 +33,11 @@
                     <div class="menu-item px-3">
                             
                             <a href="<?= $item->url ?>" class="menu-link px-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5" fill="black" />
-                                <rect x="10.8891" y="17.8033" width="12" height="2" rx="1" transform="rotate(-90 10.8891 17.8033)" fill="black" />
-                                <rect x="6.01041" y="10.9247" width="12" height="2" rx="1" fill="black" />
-                            </svg>
-                            <?= $item->title ?></a>
+                            <span class="ml-2">
+                            <?= get_post_meta( $item->ID, 'pidogame_framework_menu', true )['opt-menu-icon'] ?>
+                            </span>
+                            <?= $item->title ?>
+                        </a>
 
                     </div>
                 <?php endif; ?>
