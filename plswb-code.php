600bdda7f91aca75866e2c9e3e01f2b2e66b3c05
Your Name <you@example.com>
2022-08-13 12:00:58 +0430

    nav menu

--- plswb-code.php
+++ plswb-code.php
@@ -1091,3 +1091,10 @@ class submenu_Walker extends Walker_Nav_Menu
         $output .= "$indent</ul>\n</div>\n</a>\n</li>\n";
     }
 }
+
+
+$menu_name = 'main-menu';
+$locations = get_nav_menu_locations();
+$menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+$menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
+dd($menu_name);
