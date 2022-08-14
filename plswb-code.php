4743d19291934eb27db6b5c5df63024769bee80a
Your Name <you@example.com>
2022-08-14 19:57:11 +0430

    check order page

--- plswb-code.php
+++ plswb-code.php
@@ -1193,17 +1193,17 @@ function plswb_check_order()
             <div class="card-toolbar">
                 <ul class="nav nav-tabs nav-line-tabs nav-stretch fs-6 border-0">
                     <li class="nav-item">
-                        <a class="nav-link active">
+                        <a class="nav-link active pointer">
                             <h3>مشخصات کاربری</h3>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">
+                        <a class="nav-link pointer">
                             <h3>شماره سفارش</h3>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">
+                        <a class="nav-link pointer">
                             <h3>مشاهده وضعیت</h3>
                         </a>
                     </li>
