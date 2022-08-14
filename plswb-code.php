7199b4087f25f7b9fa801ae25164a2b3c17e12e2
Your Name <you@example.com>
2022-08-14 15:13:38 +0430

    alert

--- plswb-code.php
+++ plswb-code.php
@@ -1172,5 +1172,6 @@ function view_order_note_customer()
 
 function plswb_get_date($date)
 {
+    date_default_timezone_set('Asia/Tehran');
     return wp_date('Y/m/d - H:i', strtotime($date), 'Asia/Tehran');
 }
