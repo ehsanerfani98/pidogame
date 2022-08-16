90ae48ad69a6ab80ca5ea1add91ca940f1a85075
Your Name <you@example.com>
2022-08-16 16:18:53 +0430

    check order page

--- plswb-code.php
+++ plswb-code.php
@@ -1287,14 +1287,8 @@ function plswb_check_order()
                                 <div class="col-lg-12">
                                     <div class="row">
                                         <div class="col-lg-12">
-                                            <?php if(isset($_SESSION['order_info'])): 
-                                                $order = $_SESSION['order_info'];
-                                                ?>
-                                                <div class="card shadow-sm">
-                                                    <div class="card-body">
-<?= $order['status']; ?>
-                                                    </div>
-                                                </div>
+                                            <?php if(isset($_SESSION['order_info'])): ?>
+                                            <?php var_dump($_SESSION['order_info']); ?>
                                             <?php endif; ?>
                                         </div>
                                     </div>
