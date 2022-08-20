00584dff3c59d039b80dd35682e038e072bbab83
Your Name <you@example.com>
2022-08-20 20:03:32 +0430

    category paginate

--- category.php
+++ category.php
@@ -190,29 +190,31 @@ get_header();
                             <?php
                             endwhile;
                             ?>
-                            <!-- <div class="pagination">
+                            <div class="pagination">
                                 <?php
-                                echo paginate_links(array(
-                                    'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-                                    'total'        => $query->max_num_pages,
-                                    'current'      => max(1, get_query_var('paged')),
-                                    'format'       => '?paged=%#%',
-                                    'show_all'     => false,
-                                    'type'         => 'plain',
-                                    'end_size'     => 2,
-                                    'mid_size'     => 1,
-                                    'prev_next'    => true,
-                                    'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
-                                    'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
-                                    'add_args'     => false,
-                                    'add_fragment' => '',
-                                ));
+
+                                the_posts_pagination();
+                                // echo paginate_links(array(
+                                //     'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+                                //     'total'        => $query->max_num_pages,
+                                //     'current'      => max(1, get_query_var('paged')),
+                                //     'format'       => '?paged=%#%',
+                                //     'show_all'     => false,
+                                //     'type'         => 'plain',
+                                //     'end_size'     => 2,
+                                //     'mid_size'     => 1,
+                                //     'prev_next'    => true,
+                                //     'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
+                                //     'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
+                                //     'add_args'     => false,
+                                //     'add_fragment' => '',
+                                // ));
                                 ?>
-                            </div> -->
+                            </div>
                         <?php
                             wp_reset_postdata();
 
-                   
+
                         endif; ?>
                     </div>
                 </div>
