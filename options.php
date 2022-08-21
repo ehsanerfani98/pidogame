3261d5981df5bbab5c26da0f78e5577e3486a939
Your Name <you@example.com>
2022-08-21 22:49:43 +0430

    setting help

--- options.php
+++ options.php
@@ -500,7 +500,7 @@ if (class_exists('CSF')) {
 				'fields'		=>	array(
 					array(
 						'id'           => 'help_icon_code',
-						'type'         => 'code_editor',
+						'type'         => 'text',
 						'title'        => 'کد آیکن',
 						'sanitize' 	   => false
 					),
