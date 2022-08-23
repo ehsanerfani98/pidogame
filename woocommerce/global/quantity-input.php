6b564e62abcc033c8e18b8be9035fec4083c8caa
Your Name <you@example.com>
2022-08-23 07:14:31 +0430

    back theme

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Product quantity inputs
  *
@@ -15,37 +16,52 @@
  * @version 4.0.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( $max_value && $min_value === $max_value ) {
-	?>
+if ($max_value && $min_value === $max_value) {
+?>
 	<div class="quantity hidden">
-		<input type="hidden" id="<?php echo esc_attr( $input_id ); ?>" class="qty" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $min_value ); ?>" />
+		<input type="hidden" id="<?php echo esc_attr($input_id); ?>" class="qty" name="<?php echo esc_attr($input_name); ?>" value="<?php echo esc_attr($min_value); ?>" />
 	</div>
-	<?php
+<?php
 } else {
 	/* translators: %s: Quantity. */
-	$label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'woocommerce' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'woocommerce' );
-	?>
-	<div class="quantity">
-		<?php do_action( 'woocommerce_before_quantity_input_field' ); ?>
-		<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
-		<input
+	$label = !empty($args['product_name']) ? sprintf(esc_html__('%s quantity', 'woocommerce'), wp_strip_all_tags($args['product_name'])) : esc_html__('Quantity', 'woocommerce');
+?>
+	<div class=" position-relative w-100px d-inline-block">
+		<?php do_action('woocommerce_before_quantity_input_field'); ?>
+		<!-- <label class="screen-reader-text" for="<?php echo esc_attr($input_id); ?>"><?php echo esc_attr($label); ?></label> -->
+
+		<div data-kt-dialer="true" data-kt-dialer-min="<?php echo esc_attr($min_value); ?>" data-kt-dialer-max="<?php echo esc_attr(0 < $max_value ? $max_value : ''); ?>" data-kt-dialer-step="<?php echo esc_attr($step); ?>">
+			<button type="button" class="btn btn-icon btn-active-color-gray-700 position-absolute translate-middle-y top-50 start-0" data-kt-dialer-control="decrease"><span class="svg-icon svg-icon-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+						<rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5" fill="currentColor"></rect>
+						<rect x="6.0104" y="10.9247" width="12" height="2" rx="1" fill="currentColor"></rect>
+					</svg></span></button>
+			<input id="<?php echo esc_attr($input_id); ?>" name="<?php echo esc_attr($input_name); ?>" value="<?php echo esc_attr($input_value); ?>" type="text" class="form-control form-control-solid border-0 text-center ss02 w-100px" readonly data-kt-dialer-control="input">
+			<button type="button" class="btn btn-icon btn-active-color-gray-700 position-absolute translate-middle-y top-50 end-0" data-kt-dialer-control="increase"><span class="svg-icon svg-icon-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+						<rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5" fill="currentColor"></rect>
+						<rect x="10.8891" y="17.8033" width="12" height="2" rx="1" transform="rotate(-90 10.8891 17.8033)" fill="currentColor"></rect>
+						<rect x="6.01041" y="10.9247" width="12" height="2" rx="1" fill="currentColor"></rect>
+					</svg></span></button>
+
+		</div>
+		
+		<!-- <input
 			type="number"
-			id="<?php echo esc_attr( $input_id ); ?>"
-			class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
-			step="<?php echo esc_attr( $step ); ?>"
-			min="<?php echo esc_attr( $min_value ); ?>"
-			max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
-			name="<?php echo esc_attr( $input_name ); ?>"
-			value="<?php echo esc_attr( $input_value ); ?>"
-			title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ); ?>"
+			id="<?php echo esc_attr($input_id); ?>"
+			class="<?php echo esc_attr(join(' ', (array) $classes)); ?>"
+			step="<?php echo esc_attr($step); ?>"
+			min="<?php echo esc_attr($min_value); ?>"
+			max="<?php echo esc_attr(0 < $max_value ? $max_value : ''); ?>"
+			name="<?php echo esc_attr($input_name); ?>"
+			value="<?php echo esc_attr($input_value); ?>"
+			title="<?php echo esc_attr_x('Qty', 'Product quantity input tooltip', 'woocommerce'); ?>"
 			size="4"
-			placeholder="<?php echo esc_attr( $placeholder ); ?>"
-			inputmode="<?php echo esc_attr( $inputmode ); ?>"
-			autocomplete="<?php echo esc_attr( isset( $autocomplete ) ? $autocomplete : 'on' ); ?>"
-		/>
-		<?php do_action( 'woocommerce_after_quantity_input_field' ); ?>
+			placeholder="<?php echo esc_attr($placeholder); ?>"
+			inputmode="<?php echo esc_attr($inputmode); ?>"
+			autocomplete="<?php echo esc_attr(isset($autocomplete) ? $autocomplete : 'on'); ?>"
+		/> -->
+		<?php do_action('woocommerce_after_quantity_input_field'); ?>
 	</div>
-	<?php
+<?php
 }
--- woocommerce/notices/notice.php
+++ woocommerce/notices/notice.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Show messages
  *
@@ -15,18 +16,38 @@
  * @version 3.9.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-if ( ! $notices ) {
+if (!$notices) {
 	return;
 }
 
 ?>
 
-<?php foreach ( $notices as $notice ) : ?>
-	<div class="woocommerce-info"<?php echo wc_get_notice_data_attr( $notice ); ?>>
-		<?php echo wc_kses_notice( $notice['notice'] ); ?>
+<?php foreach ($notices as $notice) : ?>
+	<div class="alert alert-dismissible bg-light-info border-dashed border-1 border-info d-flex flex-column flex-sm-row w-100 p-5 align-items-center" <?php echo wc_get_notice_data_attr($notice); ?>>
+		<!--begin::Icon-->
+		<!--begin::Svg Icon | path: icons/duotune/general/gen007.svg-->
+		<span class="svg-icon svg-icon-2hx svg-icon-info me-4 mb-5 mb-sm-0">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+				<path opacity="0.3" d="M12 22C13.6569 22 15 20.6569 15 19C15 17.3431 13.6569 16 12 16C10.3431 16 9 17.3431 9 19C9 20.6569 10.3431 22 12 22Z" fill="black"></path>
+				<path d="M19 15V18C19 18.6 18.6 19 18 19H6C5.4 19 5 18.6 5 18V15C6.1 15 7 14.1 7 13V10C7 7.6 8.7 5.6 11 5.1V3C11 2.4 11.4 2 12 2C12.6 2 13 2.4 13 3V5.1C15.3 5.6 17 7.6 17 10V13C17 14.1 17.9 15 19 15ZM11 10C11 9.4 11.4 9 12 9C12.6 9 13 8.6 13 8C13 7.4 12.6 7 12 7C10.3 7 9 8.3 9 10C9 10.6 9.4 11 10 11C10.6 11 11 10.6 11 10Z" fill="black"></path>
+			</svg>
+		</span>
+		<!--end::Svg Icon-->
+		<!--end::Icon-->
+		<!--begin::Content-->
+		<div class="d-flex flex-column pe-0 pe-sm-10">
+			<span>
+				<?php echo wc_kses_notice($notice['notice']); ?>
+			</span>
+		</div>
+		<!--end::Content-->
+
 	</div>
+	<!-- <div class="woocommerce-info" <?php echo wc_get_notice_data_attr($notice); ?>>
+		<?php echo wc_kses_notice($notice['notice']); ?>
+	</div> -->
 <?php endforeach; ?>
