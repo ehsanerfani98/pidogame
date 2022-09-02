2714af8b7d72253c8af8196976a10b725390e44f
Your Name <you@example.com>
2022-09-03 04:21:20 +0430

    add widget to elementor

--- inc/widgets-elementor/view-widgets/Products_view.php
+++ inc/widgets-elementor/view-widgets/Products_view.php
@@ -1,13 +1,7 @@
-<?php
 
-echo $term_id."<br>";
-echo $count."<br>";
-
-?>
-<!--  -->
-<!-- <section class="splide" aria-labelledby="carousel-heading">
+<section class="splide" aria-labelledby="carousel-heading">
   <div class="splide__track">
-    <ul class="splide__list"> -->
+    <ul class="splide__list">
       <?php
 
       $args = array(
@@ -29,20 +23,36 @@ echo $count."<br>";
           $query->the_post();
           $product = wc_get_product(get_the_ID());
 
-          the_title( );
 
       ?>
-       
+          <li class="splide__slide py-5">
+            <div class="card plswb-card-yellow">
+
+              <div class="imgBox">
+                <?php the_post_thumbnail() ?>
+              </div>
+
+              <div class="contentBox">
+                <h3 class="text-gray-600"><?php the_title() ?></h3>
+                <h2 class="price"><?= number_format($product->get_price()) .' '. get_woocommerce_currency_symbol() ?></h2>
+                <a href="#" class="buy">افزودن به سبد خرید</a>
+              </div>
+
+            </div>
+          </li>
 
       <?php
         endwhile;
         wp_reset_postdata();
-      endif;
 
-     
+      endif;
       ?>
 
-    <!-- </ul>
+    </ul>
 
   </div>
-</section> -->
+</section>
+
+
+
+
