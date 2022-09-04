ad8ac68dbf4ddf2e3f1536bb25e64156a4ad7dbf
Your Name <you@example.com>
2022-09-05 01:59:27 +0430

    widget product

--- inc/widgets-elementor/functions-widgets/Products.php
+++ inc/widgets-elementor/functions-widgets/Products.php
@@ -236,7 +236,7 @@ class Products extends \Elementor\Widget_Base
 	protected function render()
 	{
 		$settings = $this->get_settings_for_display();
-		dd($settings['icon_card']);
+		dd($settings['icon_card']['value']);
 
 		$product_ids = implode(',', $settings['product_ids']);
 ?>
