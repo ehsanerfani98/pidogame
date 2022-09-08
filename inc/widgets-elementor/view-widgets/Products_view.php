c776c917c8f0b62951744fe395d207b652ce9c3a
Your Name <you@example.com>
2022-09-08 12:31:02 +0430

    edit widget product

--- inc/widgets-elementor/view-widgets/Products_view.php
+++ inc/widgets-elementor/view-widgets/Products_view.php
@@ -451,7 +451,7 @@ if ($card_style == 'festival') : ?>
                               }
                             }
 
-                            var_dump(array_unique($percents));
+                            $percentage = max(array_unique($percents));
                           }
                           if ($percentage != 0) :
                           ?>
