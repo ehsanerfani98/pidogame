dd99845697dbbaff74b1f8afaa37710910d4a469
Your Name <you@example.com>
2022-09-08 12:22:02 +0430

    edit widget product

--- inc/widgets-elementor/view-widgets/Products_view.php
+++ inc/widgets-elementor/view-widgets/Products_view.php
@@ -444,10 +444,8 @@ if ($card_style == 'festival') : ?>
                         } else {
                           if (count((new WC_Product_Variable(get_the_ID()))->get_children()) > 0) {
                             foreach ($product->get_available_variations() as $variation) {
-                              $variation_price = get_variation_price_by_id($product->ID ,$variation['variation_id']);
-                              $price = $variation_price->display_regular_price;
-                              $sale_price  = $variation_price->display_price;
-
+                              $price = $variation['display_regular_price'];
+                              $sale_price = $variation['display_price'];
                               $percentage = intval((($price - $sale_price) / $sale_price) * 100);
                               $percents[] = $percentage;
                             }
