fd75a35821bc471179e2403cd9403824a14838f5
Your Name <you@example.com>
2022-09-09 11:52:52 +0430

    percentage

--- inc/widgets-elementor/view-widgets/Products_view.php
+++ inc/widgets-elementor/view-widgets/Products_view.php
@@ -428,9 +428,9 @@ if ($card_style == 'festival') : ?>
                   <div>
                   <?php endif; ?>
 
-                  <?php if ($status_slider == 'yes') { ?>
+                  <? if ($status_slider == 'yes') : ?>
                     <div class="col-lg-3 mb-5">
-                    <?php } ?>
+                    <?php endif; ?>
 
                     <a href="<?php the_permalink() ?>">
                       <div class="wrap-cart-plswb card">
@@ -560,9 +560,9 @@ if ($card_style == 'festival') : ?>
 
                     </a>
 
-                    <?php if ($status_slider == 'yes') { ?>
+                    <!-- <? if ($status_slider == 'yes') : ?> -->
                     </div>
-                    <?php } ?>
+                  <!-- <?php endif; ?> -->
 
                   <? if ($status_slider != 'yes') : ?>
                   </div>
