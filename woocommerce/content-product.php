be5136932d814dda491eed47ef3d0ebcbf9dbd5e
Your Name <you@example.com>
2022-09-10 14:22:31 +0430

    update sale filter

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -27,7 +27,8 @@ if (empty($product) || !$product->is_visible()) {
 
 $meta = get_post_meta(get_the_ID(), 'pidogame_framework_products', true);
 ?>
-<div class="col-lg-3 mb-8">
+<div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-8">
+
 	<a href="<?php the_permalink() ?>">
 		<div class="wrap-cart-plswb card">
 
