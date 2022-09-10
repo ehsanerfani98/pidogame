b083e712e84566d2b280b42acb626a168493b84b
Your Name <you@example.com>
2022-09-10 17:29:50 +0430

    responsive

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -43,7 +43,7 @@ $meta = get_post_meta(get_the_ID(), 'pidogame_framework_products', true);
 
 			<div class="wrap-content-product mt-2 px-4 py-3">
 				<div class="title-cart-plswb">
-					<h4><?= substr(get_the_title(),0,40,'...') ?></h4>
+					<h4><?= wp_trim_words(get_the_title(), 5, ' ... '); ?></h4>
 				</div>
 
 				<div class="device-cart-plswb">
