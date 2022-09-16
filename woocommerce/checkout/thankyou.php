697259bfd91f4d66ba87fd85b72a2e4d15c59035
Your Name <you@example.com>
2022-09-16 13:38:27 +0430

    login style edit

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -40,7 +40,7 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php else : ?>
 
-			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received bg-gray-100 p-5"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received bg-gray-100 p-5 rounded"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 
 			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details bg-success p-5 rounded text-white">
 
