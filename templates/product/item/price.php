2cbcef452e0d6b4deadeaea20ea42844b73e8852
Your Name <you@example.com>
2022-09-19 16:26:03 +0430

    edit style

--- templates/product/item/price.php
+++ templates/product/item/price.php
@@ -250,7 +250,7 @@ $type = $product->get_type();
         <?php endif;
         break;
     case 'variable': ?>
-        <div class="scroll h-400px pe-5 bg-gray-400">
+        <div class="scroll h-400px pe-5 bg-gray-800">
             <?php $variations = $product->get_available_variations();
             foreach ($variations as $variation) :
                 $description = $variation['variation_description'];
