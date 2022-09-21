e3ac34d44822e3f38137e2fbd840f6bc1d19e88a
Your Name <you@example.com>
2022-09-21 16:17:39 +0430

    edit site

--- templates/page/header/header.php
+++ templates/page/header/header.php
@@ -23,10 +23,13 @@
     
 </div>
 
+<div id="kt_header" class="header align-items-stretch" data-kt-sticky="true" data-kt-sticky-name="header" data-kt-sticky-offset="{default: '200px', lg: '300px'}">
+
 <div class="header-container container-xxl">
         <div class="d-flex align-items-stretch" id="kt_header_nav">
             <div class="header-menu align-items-stretch h-lg-75px" data-kt-drawer="true" data-kt-drawer-name="header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
                 <?php get_template_part('templates/page/header/menu') ?>
             </div>
         </div>
+    </div>
     </div>
