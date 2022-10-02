70f55827af251f209dab055f9adfc5cd0bf87d6f
Your Name <you@example.com>
2022-10-02 13:47:00 +0330

    test cart

--- functions.php
+++ functions.php
@@ -11,8 +11,6 @@ define('IMAGES_URL', get_stylesheet_directory_uri() . '/assets/media/images/');
 function mytheme_add_woocommerce_support()
 {
 	add_theme_support('woocommerce');
-	remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-	remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 }
 add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
 
