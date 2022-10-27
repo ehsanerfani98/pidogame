d58b11522010d8967c0d9a302a01f2a9298bd3ca
Your Name <you@example.com>
2022-10-27 10:58:37 +0330

    edit header

--- woocommerce/myaccount/view-order.php
+++ woocommerce/myaccount/view-order.php
@@ -22,20 +22,7 @@ defined('ABSPATH') || exit;
 
 $notes = $order->get_customer_order_notes();
 ?>
-<p>
-	<?php
 
-	do_action('insert_style_order_dashboard');
-
-	printf(
-		/* translators: 1: order number 2: order date 3: order status */
-		esc_html__('Order #%1$s was placed on %2$s and is currently %3$s.', 'woocommerce'),
-		'<mark class="order-number">' . $order->get_order_number() . '</mark>', // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		'<mark class="order-date">' . wc_format_datetime($order->get_date_created()) . '</mark>', // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		'<mark class="order-status">' . wc_get_order_status_name($order->get_status()) . '</mark>' // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	);
-	?>
-</p>
 
 <?php if ($notes) : ?>
 	<h2><?php esc_html_e('Order updates', 'woocommerce'); ?></h2>
