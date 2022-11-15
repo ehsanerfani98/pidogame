58f915acffa9e180f84c3b7b278a7ac3f372a921
Your Name <you@example.com>
2022-11-15 15:30:25 +0330

    cart edit

--- assets/js/plswb-js.js
+++ assets/js/plswb-js.js
@@ -255,10 +255,11 @@ jQuery.each(status_collection, function (index, value) {
 jQuery(document).ready(function ($) {
     
     $('.increase').click(function(){
-        console.log('increase');
+        console.log('increase',$(this).parent().find('.plswb-quantity').text());
         var current_number = parseInt($(this).parent().find('.plswb-quantity').text()) + 1;
         $(this).parent().find('.plswb-quantity').text(current_number)
     });
+
     $('.decrease').click(function(){
         console.log('decrease');
         var current_number = parseInt($(this).parent().find('.plswb-quantity').text()) - 1;
