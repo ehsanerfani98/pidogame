14de31b3950b3d838b77b9e9146a7372deee37b7
Your Name <you@example.com>
2022-11-15 14:55:31 +0330

    cart edit

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -47,7 +47,7 @@ if ($max_value && $min_value === $max_value) {
 		</div> -->
 		<div class="wrap-input-number">
 
-			<button>
+			<button type="button">
 				<span class="svg-icon svg-icon-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 						<rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5" fill="currentColor"></rect>
 						<rect x="6.0104" y="10.9247" width="12" height="2" rx="1" fill="currentColor"></rect>
