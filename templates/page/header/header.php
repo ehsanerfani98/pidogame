b13bed8da0ea31f489f6deced3250adc377eccd3
Mahdi <mahdi77yousefi2015@gmail.com>
2022-12-06 19:03:49 +0330

    تغییرات سینگل محصول

--- templates/page/header/header.php
+++ templates/page/header/header.php
@@ -12,7 +12,7 @@
         </div>
         <?php get_template_part('templates/page/header/logo') ?>
         <div class="d-flex align-items-stretch justify-content-end flex-lg-grow-1">
-            <div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
+            <div class="menu menu-lg-rounded <!--menu-column--> menu-lg-row menu-state-bg menu-title-gray-700 menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
                 <?php
                 $menu = headerMenuArray();
                 foreach ($menu as $menuItem) :
